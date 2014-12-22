6a763a58fd6d6663fc6e8bb07bab6403553b11b8
Maniteja Nandana <manitejanmt@gmail.com>
2014-12-22 05:57:51 +0530

    #5354 Added the  testcase for clip-fft in test_regression.py

--- numpy/core/tests/test_regression.py
+++ numpy/core/tests/test_regression.py
@@ -2105,14 +2105,14 @@ class TestRegression(TestCase):
         assert_equal(np.array([10]) == x, "OK")
 
     def test_clip_method_crash(self):
-	## Check segfault reported in gh-5354 doesn't occur anymore
-	size = 256
-	t = np.linspace(start=0, stop=50*np.pi, endpoint=False, num=size)
-	x,y = np.meshgrid(t, t)
-	img = (127 + 127*np.sin(x)).astype(np.uint8)
-	F = fft2(img)
-	F_min = F.min()
-	F = F.clip(F_min)
+	## Check segfault reported in gh-5354 doesn't occur anymor
+        size = 256
+        t = np.linspace(start=0, stop=50*np.pi, endpoint=False, num=size)
+        x,y = np.meshgrid(t, t)
+        img = (127 + 127*np.sin(x)).astype(np.uint8)
+        F = fft2(img)
+        F_min = F.min()
+        F = F.clip(F_min)
 
 
 if __name__ == "__main__":
